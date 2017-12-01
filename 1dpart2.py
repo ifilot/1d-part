ce4e830a090af3a2ac75441cf768448ba4535fa9
ifilot <ivo@zuidstijl.nl>
2017-12-01 14:14:30 +0100

    Fixing semi-conserving solution

--- 1dpart2.py
+++ 1dpart2.py
@@ -19,29 +19,19 @@ beta = D * delta_t / delta_r**2
 
 # construct arrays
 r = numpy.linspace(delta_r / 2, r_part - delta_r / 2, num=N)
-ae = numpy.zeros([N,])
-aw = numpy.zeros([N,])
-ap = numpy.zeros([N,])
-A = numpy.zeros([N,N])
-for i in range(0,N):
-	if i > 0:
-		aw[i] = beta * (0.5*(r[i-1]+r[i]))**2 / r[i]**2		# west side
-	if i < N-1:
-		ae[i] = beta * (0.5*(r[i+1]+r[i]))**2 / r[i]**2		# east side
-	if i > 0 and i < N-1:
-		ap[i] = 1 + aw[i] + ae[i]		# center
 
 # construct matrices
+A = numpy.zeros([N,N])
 for i in range(1,N-1):
-	A[i,i] = ap[i]
-	A[i,i-1] = -aw[i]
-	A[i,i+1] = -ae[i]
+	A[i,i] = 1 + 2 * beta
+	A[i,i-1] = - beta * (1 - delta_r / r[i])
+	A[i,i+1] = - beta * (1 + delta_r / r[i])
 
 # these follow from the boundary conditions
-A[0,0] = 1 + ae[0] * 2
-A[0,1] = -ae[0] * 2
-A[N-1,N-1] = 1 + aw[N-1] + 2 * beta * r_part**2 / r[N-1]**2
-A[N-1,N-2] = -aw[N-1]
+A[0,0] = 1 + 2 * beta
+A[0,1] = - 2 * beta
+A[N-1,N-1] = 1 + 2 * beta
+A[N-1,N-2] = - beta * (1 - delta_r / r[i])
 
 # construct production matrices
 c_prev = numpy.ones([N,]) * c_0
@@ -49,9 +39,9 @@ c_prev = numpy.ones([N,]) * c_0
 # populate solution vectors
 for i in range(0,1000):
 	c_prod = -k * c_prev * delta_t
-	c_prod[N-1] += 2 * beta * c_s * r_part**2 / r[N-1]**2
+	c_prod[N-1] += beta * (1 + delta_r / r_part)
 	c_new = numpy.linalg.solve(A,c_prev + c_prod)
-	if numpy.linalg.norm(c_prev - c_new,2) < 1e-9:
+	if numpy.linalg.norm(c_prev - c_new,2) < 1e-6:
 		print "Convergence reached after %i steps" % i
 		break
 	c_prev = c_new
@@ -67,6 +57,6 @@ c_a[0] = math.sqrt(k/D) / math.sinh(r_part * math.sqrt(k/D)) * r_part # here we 
 # plot results
 plt.plot(r,c_new,'--o',label='Numerical')
 plt.plot(r_a,c_a,'--',label='Analytical')
-plt.axis((0,1,0,1))
+#plt.axis((0,1,0,1))
 plt.legend(loc=4)
 plt.show()
